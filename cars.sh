c09a14db50a0feb725b7977628aa7320c4e5a28d
Minna Yu <minyu@chapman.edu>
2020-12-06 15:54:56 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+# cars.sh
+# Minna Yu
+
+echo "Please choose one of the following by entering a number: "; 
+read -r choice
+while [[ "$choice" != "3" ]]
+do
+	echo "Type 1 to enter a new car"
+	echo "Type 2 to display the list of cars"
+	echo "Type 3 to quit and exit the program."
+	case "$choice" in
+		1) 
+			echo "What year is the car?"
+			read year
+			echo "What is the make?"
+			read make
+			echo "What is the model?"
+			read model
+			a=":"
+			combine="$year$a$make$a$model"
+			echo $combine >> My_old_cars.txt
+			;;
+
+		2) 
+			echo "Here is your inventory of cars: "
+			sort My_old_cars.txt
+			# echo "$(<My_old_cars.txt)"
+			echo "End of file"
+			;;
+
+		3) 
+			echo "Goodbye" exit
+			;;
+	esac
+done
+
+
